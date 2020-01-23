4c24b40d4e56f776ff28ebc8e39be1f576f5fc2a
Adela Darmansyah and Jiwon Han <adela@>
2020-01-23 11:53:50 -0800

    Added specs for the four and five

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
       
       var input = $("#user-input").val().split("");
       //console.log(input)
-
+      var inputcombined = input.join("")
+     // console.log(inputcombined)
       var chars = [];
 
       var vowels = /[aeiou]/i;
       var consonants = /[bcdfghjklmnpqrstvwxyz]/i;
+      var qu = /q.*?u/i;
 
      //for (var i = 0; i < input.length; i++) {
         if (input.length === 1 && input[0].match(vowels)) {
@@ -16,18 +18,25 @@ $(document).ready(function(){
         } else if (input.length > 1 && input[0].match(vowels)) {
           chars.push(input.join("") + "way")
         } else if (input.length > 1 && input[0].match(consonants)) {
-          for (var i = 0; i < input.length; i++) {
-            if (input[i].match(vowels)) {
-              var indeoxOfFirstVowel = input.indexOf(input[i]);
-              console.log(indeoxOfFirstVowel);
-              break;
-            } 
+          if (inputcombined.match(qu)) {
+            var indexOfFirstQU = inputcombined.match(qu).index;
+            console.log(indexOfFirstQU)
+            var splicedOut = input.splice(0, indexOfFirstQU+2);
+            console.log(splicedOut);
+            chars.push(input.join("") + splicedOut.join("") + "ay");
+          } else {
+            for (var i = 0; i < input.length; i++) {
+              if (input[i].match(vowels)) {
+                var indexOfFirstVowel = input.indexOf(input[i]);
+                console.log(indexOfFirstVowel);
+                var splicedOut = input.splice(0, indexOfFirstVowel);
+                console.log(splicedOut);
+                chars.push(input.join("") + splicedOut.join("") + "ay");
+                break;
+              } 
+            }
           }
-          // var splicedOut = input.splice(0, 1);
-          // chars.push(input.join("") + splicedOut + "ay");
         } 
-            
-
           
      //} 
 
