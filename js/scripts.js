899e06074b7f1a30e7dd9d74f7d61bcc9be052e8
Adela Darmansyah and Jiwon Han <adela@>
2020-01-23 16:42:39 -0800

    Attempt to remove special characters from output

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
       var original = $("#user-input").val()
       var inputPerWords = $("#user-input").val().split(" ")
       var inputPerChar = $("#user-input").val().split("");
-      console.log(original)
-      console.log(inputPerWords)
-      console.log(inputPerChar)
+      // console.log(original)
+      // console.log(inputPerWords)
+      // console.log(inputPerChar)
       var inputcombined = inputPerChar.join("")
      // console.log(inputcombined)
       var chars = [];
@@ -15,15 +15,18 @@ $(document).ready(function(){
       var vowels = /[aeiou]/i;
       var consonants = /[bcdfghjklmnpqrstvwxyz]/i;
       var qu = /q.*?u/i;
+      
 
-      if (inputPerWords.length > 1){
+
+      var translateToPigLatin = function(inputPerWords, inputPerChar) {
+        if (inputPerWords.length > 1){
         
         for (var i = 0; i < inputPerWords.length; i++) {
-          console.log(inputPerWords[1]);
+          // console.log(inputPerWords[1]);
           var inputPerLetter = inputPerWords[i].split("");
-          console.log(inputPerLetter[0]);
+          // console.log(inputPerLetter[0]);
           var inputPerLetterCombined = inputPerLetter.join("");
-          console.log(inputPerLetterCombined);
+          // console.log(inputPerLetterCombined);
 
           // code below is to check a sentence
           if (inputPerLetter.length === 1 && inputPerLetter[0].match(vowels)) { // checks if there's only 1 letter in 1 word
@@ -33,21 +36,21 @@ $(document).ready(function(){
           } else if (inputPerLetter.length > 1 && inputPerLetter[0].match(consonants)) { // checks if the beginning of 1 word is 1 consonant
             if (inputPerLetterCombined.match(qu)) { // if the beginning of 1 word is 1 consonant, check if there is any "qu"
               var indexOfFirstQU = inputPerLetterCombined.match(qu).index;
-              console.log(indexOfFirstQU)
+              // console.log(indexOfFirstQU)
               var splicedOut = inputPerLetter.splice(0, indexOfFirstQU+2);
               splicedOut[0] = splicedOut[0].toUpperCase();
-              console.log(splicedOut)
+              // console.log(splicedOut)
               chars.push(inputPerLetter.join("") + splicedOut.join("") + "ay");
             } else { // if the beginning of 1 word is 1 consonant, then:
               for (var j = 0; j < inputPerLetter.length; j++) { // find 1 vowel and slice from the beginning until the vowel's position
                 if (inputPerLetter[j].match(vowels)) {
                   var indexOfFirstVowel = inputPerLetter.indexOf(inputPerLetter[j]);
-                  console.log(indexOfFirstVowel);
+                  // console.log(indexOfFirstVowel);
                   var splicedOut = inputPerLetter.splice(0, indexOfFirstVowel);
-                  console.log(splicedOut);
+                  // console.log(splicedOut);
                   splicedOut[0] = splicedOut[0].toUpperCase();
                   chars.push(inputPerLetter.join("") + splicedOut.join("") + "ay");
-                  console.log(chars);
+                  // console.log(chars);
                   break;
                 } 
               }
@@ -55,46 +58,66 @@ $(document).ready(function(){
           } 
         }
        
-      // code below is to check for 1 leter or 1 word only  
-      } else {
-        if (inputPerChar.length === 1 && inputPerChar[0].match(vowels)) {
-          chars.push(inputPerChar[0] + "Ay");
-        } else if (inputPerChar.length > 1 && inputPerChar[0].match(vowels)) {
-          chars.push(inputPerChar.join("") + "Way")
-        } else if (inputPerChar.length > 1 && inputPerChar[0].match(consonants)) {
-          if (inputcombined.match(qu)) {
-            var indexOfFirstQU = inputcombined.match(qu).index;
-            console.log(indexOfFirstQU)
-            var splicedOut = inputPerChar.splice(0, indexOfFirstQU+2);
-            splicedOut[0] = splicedOut[0].toUpperCase();
-            console.log(splicedOut)
-            chars.push(inputPerChar.join("") + splicedOut.join("") + "ay");
-          } else {
-            for (var k = 0; k < inputPerChar.length; k++) {
-              if (inputPerChar[k].match(vowels)) {
-                var indexOfFirstVowel = inputPerChar.indexOf(inputPerChar[k]);
-                console.log(indexOfFirstVowel);
-                var splicedOut = inputPerChar.splice(0, indexOfFirstVowel);
-                console.log(splicedOut);
-                splicedOut[0] = splicedOut[0].toUpperCase();
-                chars.push(inputPerChar.join("") + splicedOut.join("") + "ay");
-                break;
-              } 
+        // code below is to check for 1 leter or 1 word only  
+        } else {
+          if (inputPerChar.length === 1 && inputPerChar[0].match(vowels)) {
+            chars.push(inputPerChar[0] + "Ay");
+          } else if (inputPerChar.length > 1 && inputPerChar[0].match(vowels)) {
+            chars.push(inputPerChar.join("") + "Way")
+          } else if (inputPerChar.length > 1 && inputPerChar[0].match(consonants)) {
+            if (inputcombined.match(qu)) {
+              var indexOfFirstQU = inputcombined.match(qu).index;
+              // console.log(indexOfFirstQU)
+              var splicedOut = inputPerChar.splice(0, indexOfFirstQU+2);
+              splicedOut[0] = splicedOut[0].toUpperCase();
+              // console.log(splicedOut)
+              chars.push(inputPerChar.join("") + splicedOut.join("") + "ay");
+            } else {
+              for (var k = 0; k < inputPerChar.length; k++) {
+                if (inputPerChar[k].match(vowels)) {
+                  var indexOfFirstVowel = inputPerChar.indexOf(inputPerChar[k]);
+                  // console.log(indexOfFirstVowel);
+                  var splicedOut = inputPerChar.splice(0, indexOfFirstVowel);
+                  // console.log(splicedOut);
+                  splicedOut[0] = splicedOut[0].toUpperCase();
+                  chars.push(inputPerChar.join("") + splicedOut.join("") + "ay");
+                  break;
+                } 
+              }
+            }
+          } 
+        }
+        return chars;
+      }
+
+      var result = translateToPigLatin(inputPerWords, inputPerChar);
+  
+      var special = /^[a-zA-Z0-9]/gi;
+
+      console.log(result);
+      // code below does not yet work to remove special characters
+      var resultSplit = result.join(" ").split("");
+      console.log(resultSplit);
+      for (var y = 0; y < result.length; y++){
+        if (result[y].match(special)) {
+          console.log(result[y]);
+          
+          console.log(resultSplit);
+          for (var x = 0; x < resultSplit.length; x++) {
+            if (resultSplit[x].match(special)) {
+              resultSplit[x].slice();
             }
           }
-        } 
+        } else {
+          console.log("hello");
+        }
       }
-     //} 
-
-     // console.log(chars); 
-
-
       
-
+      console.log(resultSplit);
 
     
 
-      $("#result").text(chars.join(' '))
+      $("#result").text(result.join(" "));
     
     });
 });
